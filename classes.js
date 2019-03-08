37edfa6359c14faf66381b9b9182971dffca48e7
Micaiah Buttars <michael.buttars@gmail.com>
2019-03-08 13:59:24 -0700

    solved problems 1 and 2 in classes

--- classes.js
+++ classes.js
@@ -29,7 +29,19 @@
   Call your class Employee and receive all the data in the constructor in the order listed above.
 */
 
-//Code Here
+class Employee{
+  constructor(first_name, last_name, email, age){
+    this.first_name = first_name;
+    this.last_name = last_name;
+    this.email = email;
+    this.age = age;
+  }
+  makeWidget(first_name, last_name){
+    return `${this.first_name} ${this.last_name} Widget`
+    }
+
+  } 
+
 
 
 ////////// PROBLEM 2 //////////
@@ -47,7 +59,18 @@
   Call your new class Manager
 */
 
-//Code Here
+class Manager extends Employee{
+  constructor(first_name,last_name, email, age){
+    super(first_name, last_name, email, age)
+    this.reports = [];
+  }
+  hire(employee){
+    return this.reports = this.reports.push(employee)
+  }
+  fire(employee){
+    return this.reports = this.reports.splice(this.reports.index)
+  }
+}
 
 
 ////////// PROBLEM 3 //////////
